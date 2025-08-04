d6e27f02a7c96ba5c45b0289c9ca21d94e9be404
dfigurado <dilrukshanf@gmail.com>
2025-08-04 14:30:06 +0100

    Re-commit

--- backend/Domain/Entities/Product.cs
+++ backend/Domain/Entities/Product.cs
@@ -1,6 +1,12 @@
 ﻿namespace Domain.Entities;
 
-public class Product
+public class Product : BaseEntity
 {
-    
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public decimal Price { get; set; }
+    public required string ImageUrl { get; set; }
+    public required string Type { get; set; }
+    public required string Brand { get; set; }
+    public int QuantityInStock { get; set; }
 }
---
+++ backend/Infrastructure/Config/ProductConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+            builder.Property(x => x.Name).IsRequired();
+        }
+    }
+}
